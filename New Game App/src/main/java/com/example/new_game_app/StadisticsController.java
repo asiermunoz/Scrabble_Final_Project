244899376bd65a83ccf9179f0b8c7b9bf83c6c75
Ferchu <ferchualvarez1206@gmail.com>
2025-01-10 10:51:44 -0400

    Stadistics setted

--- New Game App/src/main/java/com/example/new_game_app/StadisticsController.java
+++ New Game App/src/main/java/com/example/new_game_app/StadisticsController.java
@@ -1,4 +1,37 @@
 package com.example.new_game_app;
 
-public class StadisticsController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static java.lang.String.valueOf;
+
+public class StadisticsController implements Initializable {
+
+    //java variables
+    private Player player1;
+    private Player player2;
+
+    @FXML
+    private Label player1Name;
+    @FXML
+    private Label player1Score;
+    @FXML
+    private Label player2Name;
+    @FXML
+    private Label player2Score;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        player1 = PlayerManager.player1;
+        player2 = PlayerManager.player2;
+
+        player1Name.setText(player1.getAlias());
+        player1Score.setText(String.valueOf(player1.getScore()));
+        player2Name.setText(player2.getAlias());
+        player2Score.setText(String.valueOf(player2.getScore()));
+    }
 }

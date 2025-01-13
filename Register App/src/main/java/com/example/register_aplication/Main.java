e9e330b4cc9f6040e77a79fb2f20975e7951f6c5
asiermunoz <asiermunozlandi@gmail.com>
2025-01-12 22:47:42 -0400

    New Dessing For Register User App (3)

--- Register App/src/main/java/com/example/register_aplication/Main.java
+++ Register App/src/main/java/com/example/register_aplication/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("RegisterUser.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 641, 522);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 520);
         stage.setTitle("Register User!");
         stage.setScene(scene);
         stage.show();
